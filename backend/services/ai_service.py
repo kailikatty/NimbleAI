8dba3dbf0715f317be2fe2e5e01ee57c67424d86
Narakamol.S <kailikatty@gmail.com>
2026-04-12 13:37:24 +0700

    Update ai_service.py

--- backend/services/ai_service.py
+++ backend/services/ai_service.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 
 genai.configure(api_key="YOUR_API_KEY")
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 def generate(prompt: str):
     response = model.generate_content(prompt)
