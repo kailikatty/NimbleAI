3c5357892f11e915798f691d297fb532417c9538
Narakamol.S <kailikatty@gmail.com>
2026-04-12 14:59:14 +0700

    Update ai_service.py

--- backend/services/ai_service.py
+++ backend/services/ai_service.py
@@ -2,9 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
 
 def generate(prompt: str):
     try:
