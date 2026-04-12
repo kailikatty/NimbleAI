74c05540d10d9443999bb1991069ed8786e1883d
Narakamol.S <kailikatty@gmail.com>
2026-04-12 13:52:09 +0700

    Update file.py

--- backend/routers/file.py
+++ backend/routers/file.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from services.file_analyzer import analyze_file
 
 router = APIRouter()
 
-@router.get("/")
-def test_task():
-    return {"message": "task API working"}
+class Request(BaseModel):
+    text: str
+
+@router.post("/file/analyze")
+def analyze(req: Request):
+    return {"result": analyze_file(req.text)}
