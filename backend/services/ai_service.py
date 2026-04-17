fe33c308734bbee989b9b8aa7a7a23f9abb45c15
Narakamol.S <kailikatty@gmail.com>
2026-04-17 20:06:20 +0700

    Update ai_service.py

--- backend/services/ai_service.py
+++ backend/services/ai_service.py
@@ -1,23 +1,26 @@
 import os
+import time
 from google import genai
 
 client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
 
 def generate(prompt: str):
-    try:
-        response = client.models.generate_content(
-            model="gemini-2.5-flash",
-            contents=prompt
-        )
-        # safest way
-        return getattr(response, "text", None) or str(response)
+    max_retries = 3
 
-    except Exception as e:
-        if "503" in str(e):
-            print(f"Retry {attempt+1}...")
-            time.sleep(2)  # รอ 2 วิ
-        else:
-            return f"Error: {str(e)}"
+    for attempt in range(max_retries):
+        try:
+            response = client.models.generate_content(
+                model="gemini-2.5-flash",
+                contents=prompt
+            )
+            return getattr(response, "text", None) or str(response)
+
+        except Exception as e:
+            if "503" in str(e):
+                print(f"Retry {attempt+1}...")
+                time.sleep(2)
+            else:
+                return f"Error: {str(e)}"
 
     return "Error: Server busy, please try again."
     
