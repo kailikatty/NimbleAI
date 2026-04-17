fa5c3da932a97bc318ba7d0486b4b420cbe01978
Narakamol.S <kailikatty@gmail.com>
2026-04-18 00:55:12 +0700

    Update file.py

--- backend/routers/file.py
+++ backend/routers/file.py
@@ -18,6 +18,8 @@ async def analyze(upload_file: UploadFile = File(...)):
 
     # ✅ check file size (10MB)
     contents = await upload_file.read()
+    print("📁 filename:", upload_file.filename)   # 👈 ใส่ตรงนี้
+    print("📦 file size:", len(contents))
     if len(contents) > 10 * 1024 * 1024:
         raise HTTPException(status_code=400, detail="File too large (max 10MB)")
 
