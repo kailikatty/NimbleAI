a4605aa684a50b9fae5f6bc78531d79237904036
Narakamol.S <kailikatty@gmail.com>
2026-04-18 15:31:30 +0700

    Update txt_parser.py

--- backend/services/parsers/txt_parser.py
+++ backend/services/parsers/txt_parser.py
@@ -1,7 +1,15 @@
 def extract_txt(file):
     try:
-        file.seek(0)  # 🔥 สำคัญมาก
-        return file.read().decode("utf-8")
-    except:
         file.seek(0)
-        return file.read().decode("latin-1")
+        content = file.read()
+
+        if not content:
+            return "Empty file"
+
+        try:
+            return content.decode("utf-8")
+        except:
+            return content.decode("latin-1")
+
+    except Exception as e:
+        return f"Error reading TXT file: {str(e)}"
